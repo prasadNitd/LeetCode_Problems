d2ac6d043a1916c7a24c53fe3b376bc67a25e5dd
Ezaz Hossain <hossainezaz786@gmail.com>
2020-10-19 07:19:40 +0530

    Update function and variable name

--- Day - 17/inorder_recursive.cpp
+++ Day - 17/inorder_recursive.cpp
@@ -5,14 +5,14 @@ struct TreeNode
 	int data;
 	TreeNode *left, *right;
 };
-TreeNode* insert(TreeNode* root, int val)
+TreeNode* Tree_insert(TreeNode* root, int value)
 {
 	TreeNode* temp = new TreeNode();
-	temp->data = val;
+	temp->data = value;
 	temp->left = temp->right = NULL;
 	if(!root)
 	{
-		// temp->data = val;
+		// temp->data = value;
 		// temp->left = temp->right = NULL;
 		// cout << "C ";
 		return temp;
@@ -46,13 +46,13 @@ TreeNode* insert(TreeNode* root, int val)
 	}
 	return root;
 }
-void inorder(vector<int>& arr, TreeNode *root)
+void inorder_traversal(vector<int>& arr, TreeNode *root)
 {
     if(root==NULL)
         return;
-    inorder(arr, root->left);
+    inorder_traversal(arr, root->left);
     arr.push_back(root->data);
-    inorder(arr, root->right);
+    inorder_traversal(arr, root->right);
 
 }
 int main()
@@ -64,10 +64,10 @@ int main()
 		cin >> n;
 		if(n<=0)
 			break;
-		root = insert(root, n);
+		root = Tree_insert(root, n);
 	}
 	vector<int> arr;
-	inorder(arr, root);
+	inorder_traversal(arr, root);
 	// cout << arr.size();
 	for(int i=0;i<arr.size();i++)
 	{
