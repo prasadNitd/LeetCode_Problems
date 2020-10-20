a8646ac92c6d31057ee0a31ce7aa6580ad05ab4b
ezaz039 <73117165+ezaz039@users.noreply.github.com>
2020-10-20 09:19:12 +0530

    Update function description

--- Day - 8/job_sequencing.cpp
+++ Day - 8/job_sequencing.cpp
@@ -1,61 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+int n;
 struct jobs
 {
-	int deadline, profit, id;
+    int deadline, profit, id;
 };
 
-bool compareJobs(jobs a, jobs b)
+bool cmp(jobs a, jobs b)
 {
-	return a.profit > b.profit;
+    return a.profit > b.profit;
 }
 
-int maxProfit(jobs arr[], int n, int maxDeadline)
+int max_profit(jobs arr[], int maxDeadline)
 {
-	int deadArr[maxDeadline+1] = {0}, j = -1, ans = 0;
+    int deadArr[maxDeadline+1] = {0}, j = -1, ans = 0,i;
 
-	sort(arr, arr+n, compareJobs);
+    sort(arr, arr+n, cmp);
 
-	for(int i=0;i<n;i++)
-	{
-		j = arr[i].deadline;
-		while(j)
-		{
-			if(!deadArr[j])
-			{
-				deadArr[j] = arr[i].id;
-				ans += arr[i].profit;
-				break;
-			}
-			else
-				j--;
-		}
-	}
-	for(int i=1;i<=maxDeadline;i++)
-	{
-		if(deadArr[i])
-		{
-			cout << deadArr[i] << ' ';
-		}
-	}
-	cout << endl;
-	return ans;
+    for(i=0;i<n;i++)
+    {
+        j = arr[i].deadline;
+        while(j)
+        {
+            if(!deadArr[j])
+            {
+                deadArr[j] = arr[i].id;
+                ans += arr[i].profit;
+                break;
+            }
+            else
+                j--;
+        }
+    }
+    for(i=1;i<=maxDeadline;i++)
+    {
+        if(deadArr[i])
+        {
+            cout << deadArr[i] << ' ';
+        }
+    }
+    cout << endl;
+    return ans;
 }
 
 int main()
 {
-	int n;
-	cin >> n;
+    cout << "Enter total no of job::" << endl;
+    cin >> n;
 
-	jobs arr[n];
-	int maxDeadline = -1;
-
-	for(int i=0;i<n;i++)
-	{
-		cin >> arr[i].deadline >> arr[i].profit;
-		arr[i].id = i+1;
-		maxDeadline = max(maxDeadline, arr[i].deadline);
-	}
-	cout << maxProfit(arr, n, maxDeadline);
+    jobs arr[n];
+    int maxDeadline = -1,i;
+    cout << "Enter deadline and profit of each job::" << endl;
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i].deadline >> arr[i].profit;
+        arr[i].id = i+1;
+        maxDeadline = max(maxDeadline, arr[i].deadline);
+    }
+    cout << max_profit(arr, maxDeadline);
 }
