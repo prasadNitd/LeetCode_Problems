c82465aa647cb498ee4073ce910cb4a67d5eb66c
ezaz039 <73117165+ezaz039@users.noreply.github.com>
2020-10-20 08:48:18 +0530

    Use unordered_map instead of array

--- Array and Strings/palindromePermutation.cpp
+++ Array and Strings/palindromePermutation.cpp
@@ -1,33 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool palindromePermutation(string str)
+bool check_palindrome_permutation(string str)
 {
-	int freq[26] = {0};
-
-	for(int i=0;i<str.length();i++)
-	{
-		freq[str[i]-'a'] += 1;
-	}
-	bool oddFound = false;
-	for(int i=0;i<26;i++)
-	{
-		if(freq[i]%2)
-		{
-			if(oddFound)
-				return false;
-			oddFound = true;
-		}
-	}
-	return true;
+    unordered_map<char,int> ump;
+    for(auto s:str)
+        ump[s]++;
+    bool flag = false;
+    for(auto x:ump)
+    {
+        if(x.second%2)
+        {
+            if(flag)
+                return false;
+            flag = true;
+        }
+    }
+    return true;
 }
 int main()
 {
-	string s;
-	cin >> s;
+    string str;
+    cin >> str;
 
-	if(palindromePermutation(s))
-		cout << 1 << endl;
-	else
-		cout << 0 << endl;
-	return 0;
+    if(check_palindrome_permutation(str))
+        cout << "There is a palindrome permutation exist:: " << endl;
+    else
+        cout << "There is no palindrome permutation exist:: " << endl;
+    return 0;
 }
